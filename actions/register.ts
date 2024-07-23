d82b9ad25c88996b0e8c4c60d1a746f7cea969e2
Ugzher <ugo.mateda@gmail.com>
2024-07-23 15:38:20 +0200

    2h40

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,10 @@
 "use server";
+
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
@@ -9,5 +13,21 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "Incorrect fields." };
   }
 
-  return { success: "Email sent" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "User already exists" };
+  }
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: "User created !" };
 };
